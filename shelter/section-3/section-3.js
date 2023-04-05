7c42d6e2e4305ab590138aaa6725c34e102feaa3
warp5t <dmitrij.didencko@yandex.by>
2023-04-05 20:25:31 +0300

    feat: implement carousel page main

--- shelter/section-3/section-3.js
+++ shelter/section-3/section-3.js
@@ -3,8 +3,7 @@ const arrPetsInfo = [
   {
     "name": "Jennifer",
     "img": "section-3/pets-jennifer.png",
-    "type": "Dog",
-    "breed": "Labrador",
+    "breed": "Dog - Labrador",
     "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
     "age": "2 months",
     "inoculations": ["none"],
@@ -14,8 +13,7 @@ const arrPetsInfo = [
   {
     "name": "Sophia",
     "img": "section-3/pets-sofa.jpg",
-    "type": "Dog",
-    "breed": "Shih tzu",
+    "breed": "Dog - Shih tzu",
     "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
     "age": "1 month",
     "inoculations": ["parvovirus"],
@@ -25,8 +23,7 @@ const arrPetsInfo = [
   {
     "name": "Woody",
     "img": "section-3/pets-woody.png",
-    "type": "Dog",
-    "breed": "Golden Retriever",
+    "breed": "Dog - Golden Retriever",
     "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
     "age": "3 years 6 months",
     "inoculations": ["adenovirus", "distemper"],
@@ -36,8 +33,7 @@ const arrPetsInfo = [
   {
     "name": "Scarlett",
     "img": "section-3/pets-scarlet.png",
-    "type": "Dog",
-    "breed": "Jack Russell Terrier",
+    "breed": "Dog - Jack Russell Terrier",
     "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
     "age": "3 months",
     "inoculations": ["parainfluenza"],
@@ -47,8 +43,7 @@ const arrPetsInfo = [
   {
     "name": "Katrine",
     "img": "section-3/pets-katrine.png",
-    "type": "Cat",
-    "breed": "British Shorthair",
+    "breed": "Cat - British Shorthair",
     "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
     "age": "6 months",
     "inoculations": ["panleukopenia"],
@@ -58,8 +53,7 @@ const arrPetsInfo = [
   {
     "name": "Timmy",
     "img": "section-3/pets-timmy.png",
-    "type": "Cat",
-    "breed": "British Shorthair",
+    "breed": "Cat - British Shorthair",
     "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
     "age": "2 years 3 months",
     "inoculations": ["calicivirus", "viral rhinotracheitis"],
@@ -69,8 +63,7 @@ const arrPetsInfo = [
   {
     "name": "Freddie",
     "img": "section-3/pets-fred.png",
-    "type": "Cat",
-    "breed": "British Shorthair",
+    "breed": "Cat - British Shorthair",
     "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
     "age": "2 months",
     "inoculations": ["rabies"],
@@ -80,8 +73,7 @@ const arrPetsInfo = [
   {
     "name": "Charly",
     "img": "section-3/pets-charly.jpg",
-    "type": "Dog",
-    "breed": "Jack Russell Terrier",
+    "breed": "Dog - Jack Russell Terrier",
     "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
     "age": "8 years",
     "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
@@ -113,37 +105,154 @@ let friendName = document.querySelector('.box-carousel__name-pet');
 let containerCards = document.querySelector('.box-carousel__cards-list');
 
 close.onclick = function(){
-  console.log('check');
   popUp.classList.remove('tech-visible');
   document.body.style.overflow = 'initial';
 }
 
-leftBtn.onclick = function(){
-  console.log('left button');
-}
-rigthBtn.onclick = function(){
-  console.log('right button');
+let arrRand = [];
+let cardClone;
+let counterStep = 0;
+let duratinon = 1000;
+let permissionBtn = true;
+let sizeMove = 1080;
+
+function permissing(){
+  setTimeout(() => {
+    permissionBtn = true;
+  }, duratinon + 500);
 }
 
-function randomating(){
-  let arrRand = [];
-  let cardClone;
-  let rNumber;
-    while(arrRand.length != 3){
-      cardClone = cardInit.cloneNode(true);
-      rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
-      if(!arrRand.includes(rNumber)){
+leftBtn.onclick = function(){
+  console.log('left button');
+  if(permissionBtn === true){
+     permissing();
+     permissionBtn = false;
+    if(counterStep === 0){
+      containerCards.style.transitionDuration = 'unset';
+      containerCards.style.left = (-1) * sizeMove + 'px';
+     while(arrRand.length != 6){
+       rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
+       cardAdding(rNumber,'left');
+     }
+     setTimeout(() => {
+       containerCards.style.transitionDuration = duratinon + 'ms';
+       containerCards.style.left = 0 + 'px';
+     }, 200);
+     counterStep++;
+     cardsFriend = document.querySelectorAll('.box-carousel__card');
+   }
+   else if(counterStep === 1 || counterStep === 3){
+     let counter = 0;
+
+     containerCards.style.transitionDuration = 'unset';
+     containerCards.style.left = -1080 + 'px';
+     while(counter <= 2){
+       counter++;
+      cardsFriend = document.querySelectorAll('.box-carousel__card');
+      cardsFriend[cardsFriend.length -1].remove();
+      arrRand.shift();
+     }
+     while(arrRand.length != 6){
+       rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
+       cardAdding(rNumber,'left');
+     }
+     setTimeout(() => {
+       containerCards.style.left = 0 + 'px';
+       containerCards.style.transitionDuration = duratinon + 'ms';
+     }, 100);
+     counterStep = 1;
+     }
+     else if(counterStep === 2){
+      containerCards.style.left = 0 + 'px';
+      counterStep = 3;
+      //containerCards.style.transitionDuration = duratinon + 'ms';
+     }
+     else if(counterStep === 4){
+      containerCards.style.left = 0 + 'px';
+      counterStep = 1;
+      //containerCards.style.transitionDuration = duratinon + 'ms';
+     }
+  }
+  console.log(counterStep, ' - counterStep');
+}
+
+rigthBtn.onclick = function(){
+  console.log('right button');
+  if(permissionBtn === true){
+    permissing();
+     if(counterStep == 0){
+      while(arrRand.length != 6){
+        containerCards.style.transitionDuration = 'unset';
+        rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
+        cardAdding(rNumber,'right');
+      }
+       setTimeout(() => {
+        containerCards.style.transitionDuration = duratinon + 'ms';
+        containerCards.style.left = (-1) * sizeMove + 'px';
+       }, 100);
+       counterStep = 4;
+    }
+    else if(counterStep == 1){
+      containerCards.style.left = (-1) * sizeMove + 'px';
+      containerCards.style.transitionDuration = duratinon + 'ms';
+      counterStep = 2;
+    }
+    else if(counterStep == 3){
+      containerCards.style.left = (-1) * sizeMove + 'px';
+      containerCards.style.transitionDuration = duratinon + 'ms';
+      counterStep = 2;
+    }
+    else if(counterStep == 4 || counterStep == 2){
+      let counter = 0;
+        containerCards.style.transitionDuration = 'unset';
+        containerCards.style.left = 0 + 'px';
+        while(counter <= 2){
+        counter++;
+        cardsFriend = document.querySelectorAll('.box-carousel__card');
+        cardsFriend[0].remove();
+        arrRand.shift();
+       }
+       while(arrRand.length != 6){
+        rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
+        cardAdding(rNumber,'right');
+      }
+      setTimeout(() => {
+        containerCards.style.transitionDuration = duratinon + 'ms';
+        containerCards.style.left = (-1) * sizeMove + 'px';
+      }, 100);
+    }
+  }
+  console.log(counterStep, ' - counterStep');
+}
+
+function cardAdding(rNumber,direct){
+   if(!arrRand.includes(rNumber)){
+        cardClone = cardInit.cloneNode(true);
         arrRand.push(rNumber);
         cardClone.childNodes[3].innerText = `${arrPetsInfo[rNumber].name}`;
         cardClone.childNodes[1].childNodes[1].src = `${arrPetsInfo[rNumber].img}`;
         cardClone.id = `${arrPetsInfo[rNumber].name}`;
-        containerCards.prepend(cardClone);
-    }
+        if(direct == 'left'){
+          containerCards.prepend(cardClone);
+        }
+        else if(direct == 'right'){
+          containerCards.append(cardClone);
+        }
     }
     cardsFriend = document.querySelectorAll('.box-carousel__card');
 }
 
-randomating()
+function randoming(){
+  let rNumber;
+    while(arrRand.length != 3){
+      rNumber = Math.floor(Math.random() * (arrPetsInfo.length));
+      cardAdding(rNumber,'left');
+    }
+    cardsFriend = document.querySelectorAll('.box-carousel__card');
+    cardsFriend[3].remove();
+}
+
+randoming()
 
 cardsFriend.forEach(function(item){
   item.addEventListener('click', function(){
@@ -152,7 +261,7 @@ cardsFriend.forEach(function(item){
     if(item.id == 'Katrine'){
       photo.src = arrPetsInfo[4].img;
       name.innerText = arrPetsInfo[4].name;
-      species.innerText = arrPetsInfo[4].type;
+      species.innerText = arrPetsInfo[4].breed;
       about.innerText = arrPetsInfo[4].description;
       age.innerText = arrPetsInfo[4].age;
       inocul.innerText = arrPetsInfo[4].inoculations;
@@ -162,7 +271,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Jennifer'){
       photo.src = arrPetsInfo[0].img;
       name.innerText = arrPetsInfo[0].name;
-      species.innerText = arrPetsInfo[0].type;
+      species.innerText = arrPetsInfo[0].breed;
       about.innerText = arrPetsInfo[0].description;
       age.innerText = arrPetsInfo[0].age;
       inocul.innerText = arrPetsInfo[0].inoculations;
@@ -172,7 +281,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Woody'){
       photo.src = arrPetsInfo[2].img;
       name.innerText = arrPetsInfo[2].name;
-      species.innerText = arrPetsInfo[2].type;
+      species.innerText = arrPetsInfo[2].breed;
       about.innerText = arrPetsInfo[2].description;
       age.innerText = arrPetsInfo[2].age;
       inocul.innerText = arrPetsInfo[2].inoculations;
@@ -182,7 +291,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Sophia'){
       photo.src = arrPetsInfo[1].img;
       name.innerText = arrPetsInfo[1].name;
-      species.innerText = arrPetsInfo[1].type;
+      species.innerText = arrPetsInfo[1].breed;
       about.innerText = arrPetsInfo[1].description;
       age.innerText = arrPetsInfo[1].age;
       inocul.innerText = arrPetsInfo[1].inoculations;
@@ -192,7 +301,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Timmy'){
       photo.src = arrPetsInfo[5].img;
       name.innerText = arrPetsInfo[5].name;
-      species.innerText = arrPetsInfo[5].type;
+      species.innerText = arrPetsInfo[5].breed;
       about.innerText = arrPetsInfo[5].description;
       age.innerText = arrPetsInfo[5].age;
       inocul.innerText = arrPetsInfo[5].inoculations;
@@ -202,7 +311,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Charly'){
       photo.src = arrPetsInfo[7].img;
       name.innerText = arrPetsInfo[7].name;
-      species.innerText = arrPetsInfo[7].type;
+      species.innerText = arrPetsInfo[7].breed;
       about.innerText = arrPetsInfo[7].description;
       age.innerText = arrPetsInfo[7].age;
       inocul.innerText = arrPetsInfo[7].inoculations;
@@ -212,7 +321,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Scarlett'){
       photo.src = arrPetsInfo[3].img;
       name.innerText = arrPetsInfo[3].name;
-      species.innerText = arrPetsInfo[3].type;
+      species.innerText = arrPetsInfo[3].breed;
       about.innerText = arrPetsInfo[3].description;
       age.innerText = arrPetsInfo[3].age;
       inocul.innerText = arrPetsInfo[3].inoculations;
@@ -222,7 +331,7 @@ cardsFriend.forEach(function(item){
     else if(item.id == 'Freddie'){
       photo.src = arrPetsInfo[6].img;
       name.innerText = arrPetsInfo[6].name;
-      species.innerText = arrPetsInfo[6].type;
+      species.innerText = arrPetsInfo[6].breed;
       about.innerText = arrPetsInfo[6].description;
       age.innerText = arrPetsInfo[6].age;
       inocul.innerText = arrPetsInfo[6].inoculations;
